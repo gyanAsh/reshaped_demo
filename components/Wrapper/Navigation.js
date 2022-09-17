7a15a89dea62d112e2e43af1904cb286a934b02b
gyanAsh <gyanash36@protonmail.com>
2022-09-17 11:16:47 +0530

    Updated sideNav color

--- components/Wrapper/Navigation.js
+++ components/Wrapper/Navigation.js
@@ -3,9 +3,7 @@ import Link from 'next/link'
 import { ActionBar, Button, Image, Frame, Stack } from "reshaped"
 import style from '../../styles/Navigation.module.css'
 const Navigation = () => {
-
     const router = useRouter();
-    console.log(router.asPath)
     return (
         <Frame
             className={style.navbar}
@@ -23,46 +21,45 @@ const Navigation = () => {
                 <Stack>
                     <Stack.Item>
                         <Link href="/v3/home">
-                            <Button  color={router.asPath.match("/v3/home") ? "primary" : "secondary"} fullWidth>
+                            <Button  color={router.asPath.match("/v3/home") ? "primary" : "neutral"} fullWidth>
                                 Home
                             </Button>
                         </Link>
                     </Stack.Item>
                     <Stack.Item>
                         <Link href="/v3/packs">
-                            <Button color={router.asPath.match("/v3/packs") ? "primary" : "secondary"} fullWidth>
+                            <Button color={router.asPath.match("/v3/packs") ? "primary" : "neutral"} fullWidth>
                                 Packs
                             </Button>
                         </Link>
                     </Stack.Item>
                     <Stack.Item>
                         <Link href="/v3/tests">
-                            <Button color={router.asPath.match("/v3/tests") ? "primary" : "secondary"} fullWidth>
+                            <Button color={router.asPath.match("/v3/tests") ? "primary" : "neutral"} fullWidth>
                                 Tests
                             </Button>
                         </Link>
                     </Stack.Item>
                     <Stack.Item>
                         <Link href="/v3/analytics">
-                            <Button color={router.asPath.match("/v3/analytics") ? "primary" : "secondary"} fullWidth>
+                            <Button color={router.asPath.match("/v3/analytics") ? "primary" : "neutral"} fullWidth>
                                 Analytics
                             </Button>
                         </Link>
                     </Stack.Item>
                     <Stack.Item>
                         <Link href="/v3/notebooks">
-                            <Button color={router.asPath.match("/v3/notebooks") ? "primary" : "secondary"} fullWidth>
+                            <Button color={router.asPath.match("/v3/notebooks") ? "primary" : "neutral"} fullWidth>
                                 Notebooks
                             </Button>
                         </Link>
                     </Stack.Item>
                     <Stack.Item>
                         <Link href="/v3/profile">
-                            <Button color={router.asPath.match("/v3/profile") ? "primary" : "secondary"} fullWidth>
+                            <Button color={router.asPath.match("/v3/profile") ? "primary" : "neutral"} fullWidth>
                                 Profile
                             </Button>
                         </Link>
-                        
                     </Stack.Item>
                 </Stack>
             </ActionBar>
