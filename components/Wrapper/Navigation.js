45452aae64c19f5d12a3f0660ba99b5e2d9a836b
gyanAsh <gyanash36@protonmail.com>
2022-09-17 16:01:17 +0530

    sideNavbar made interative even when the path changes

--- components/Wrapper/Navigation.js
+++ components/Wrapper/Navigation.js
@@ -4,14 +4,16 @@ import { ActionBar, Button, Image, Frame, Stack } from "reshaped"
 import style from '../../styles/Navigation.module.css'
 const Navigation = () => {
     const router = useRouter();
-    console.log(router.asPath);
+    const regex = /^(\/v3\/\b(home|packs|tests|analytics|notebooks|profile)\b)/;
+    const pathInfo = router.asPath.match(regex);
+    const path = pathInfo ? pathInfo[0] : null;
     return (
         <Frame
             className={style.navbar}
             padding={[6,0]}
             height="100vh"
             // backgroundColor="positive-faded"
-            borderColor="positive-faded"
+            borderColor="neutral-faded"
             align="center"
         >
             <Link href="/v3/home">
@@ -22,42 +24,42 @@ const Navigation = () => {
                 <Stack>
                     <Stack.Item>
                         <Link href="/v3/home">
-                            <Button  color={router.asPath.match(/\/v3\/home/) ? "primary" : "disabled"} fullWidth>
+                            <Button  color={path === "/v3/home" ? "primary" : "disabled" } variant={path === "/v3/home" ?  "" :"outline" } fullWidth>
                                 Home
                             </Button>
                         </Link>
                     </Stack.Item>
                     <Stack.Item>
                         <Link href="/v3/packs">
-                            <Button color={router.asPath.match(/\/v3\/packs/) ? "primary" : "disabled"} fullWidth>
+                            <Button color={path === "/v3/packs"? "primary" : "disabled"}  variant={path === "/v3/packs"?  "" :"outline" } fullWidth>
                                 Packs
                             </Button>
                         </Link>
                     </Stack.Item>
                     <Stack.Item>
                         <Link href="/v3/tests">
-                            <Button color={router.asPath.match(/\/v3\/tests/) ? "primary" : "disabled"} fullWidth>
+                            <Button color={path==="v3/tests" ? "primary" : "disabled"}  variant={path==="v3/tests" ?  "" :"outline" } fullWidth>
                                 Tests
                             </Button>
                         </Link>
                     </Stack.Item>
                     <Stack.Item>
                         <Link href="/v3/analytics">
-                            <Button color={router.asPath.match(/\/v3\/analytics/) ? "primary" : "disabled"} fullWidth>
+                            <Button color={path==="/v3/analytics" ? "primary" : "disabled"}  variant={path==="/v3/analytics" ?  "" :"outline" } fullWidth>
                                 Analytics
                             </Button>
                         </Link>
                     </Stack.Item>
                     <Stack.Item>
                         <Link href="/v3/notebooks">
-                            <Button color={router.asPath.match(/\/v3\/notebooks/) ? "primary" : "neutral"} fullWidth>
+                            <Button color={path=== "/v3/notebooks" ? "primary" : "neutral"}  variant={path=== "/v3/notebooks" ?  "" :"outline" } fullWidth>
                                 Notebooks
                             </Button>
                         </Link>
                     </Stack.Item>
                     <Stack.Item>
                         <Link href="/v3/profile">
-                            <Button color={router.asPath.match(/\/v3\/profile/) ? "primary" : "neutral"} fullWidth>
+                            <Button color={path=== "/v3/profile" ? "primary" : "neutral"}  variant={path=== "/v3/profile" ?  "" :"outline" } fullWidth>
                                 Profile
                             </Button>
                         </Link>
