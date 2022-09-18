10cfa1ebda8424a9401e2766610f4098e7ac1cfd
gyanAsh <gyanash36@protonmail.com>
2022-09-18 17:07:47 +0530

    Added tooltip to the navLinks

--- components/Wrapper/Navigation.js
+++ components/Wrapper/Navigation.js
@@ -1,7 +1,7 @@
 import { useState,useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link'
-import { ActionBar, Button, Image, Frame, Stack } from "reshaped"
+import { ActionBar, Button, Image, Frame, Stack,Tooltip  } from "reshaped"
 import style from '../../styles/Navigation.module.css'
 
 const Navigation = () => {
@@ -25,56 +25,83 @@ const Navigation = () => {
             borderColor="neutral-faded"
             align="center"
         >
-            <Link href="/v3/home">
-                <Button variant="ghost">
-                    <Image src="/Icon/Logo.svg" alt="Vercel Logo" width="50px" />
-                </Button>
-            </Link>
-            
+            <Tooltip text="Home" position='end'>
+                { (msg)=> (
+                    <Link href="/v3/home">
+                        <Button variant="ghost" attributes={msg}>
+                            <Image src="/Icon/Logo.svg" alt="Vercel Logo" width="50px" />
+                        </Button>
+                    </Link>
+                )}
+            </Tooltip>
             <ActionBar className={style.navLinkContainer}>
                 <Stack>
-                    <Stack.Item>
-                        <Link href="/v3/home">
-                            <Button  color={path === "/v3/home" ? "primary" : "disabled" } variant={path === "/v3/home"?  "" :"outline" } fullWidth>
-                                Home
-                            </Button>
-                        </Link>
-                    </Stack.Item>
-                    <Stack.Item>
-                        <Link href="/v3/packs">
-                            <Button color={path === "/v3/packs"? "primary" : "disabled"}  variant={path === "/v3/packs"?  "" :"outline" } fullWidth>
-                                Packs
-                            </Button>
-                        </Link>
-                    </Stack.Item>
-                    <Stack.Item>
-                        <Link href="/v3/tests">
-                            <Button variant={path==="/v3/tests" ?  "" :"outline" } color={path==="/v3/tests" ? "primary" : "disabled"}   fullWidth>
-                                Tests
-                            </Button>
-                        </Link>
-                    </Stack.Item>
-                    <Stack.Item>
-                        <Link href="/v3/analytics">
-                            <Button color={path==="/v3/analytics" ? "primary" : "disabled"}  variant={path==="/v3/analytics" ?  "" :"outline" } fullWidth>
-                                Analytics
-                            </Button>
-                        </Link>
-                    </Stack.Item>
-                    <Stack.Item>
-                        <Link href="/v3/notebooks">
-                            <Button color={path=== "/v3/notebooks" ? "primary" : "neutral"}  variant={path=== "/v3/notebooks" ?  "" :"outline" } fullWidth>
-                                Notebooks
-                            </Button>
-                        </Link>
-                    </Stack.Item>
-                    <Stack.Item>
-                        <Link href="/v3/profile">
-                            <Button color={path=== "/v3/profile" ? "primary" : "neutral"}  variant={path=== "/v3/profile" ?  "" :"outline" } fullWidth>
-                                Profile
-                            </Button>
-                        </Link>
-                    </Stack.Item>
+                    <Tooltip text="Home" position='end'>
+                        { (msg)=> (
+                        <Stack.Item attributes={msg}>
+                            <Link href="/v3/home">
+                                <Button  color={path === "/v3/home" ? "primary" : "disabled" } variant={path === "/v3/home"?  "" :"outline" } fullWidth>
+                                    Home
+                                </Button>
+                            </Link>
+                        </Stack.Item>
+                        )}
+                    </Tooltip>
+                    <Tooltip text="Packs" position='end'>
+                        { (msg)=> (
+                        <Stack.Item attributes={msg}>
+                            <Link href="/v3/packs">
+                                <Button color={path === "/v3/packs"? "primary" : "disabled"}  variant={path === "/v3/packs"?  "" :"outline" } fullWidth>
+                                    Packs
+                                </Button>
+                            </Link>
+                        </Stack.Item>
+                        )}
+                    </Tooltip>
+                    <Tooltip text="Tests" position='end'>
+                        { (msg)=> (
+                        <Stack.Item attributes={msg}>
+                            <Link href="/v3/tests">
+                                <Button color={path==="/v3/tests" ? "primary" : "disabled"}  variant={path==="/v3/tests" ?  "" :"outline" } fullWidth>
+                                    Tests
+                                </Button>
+                            </Link>
+                        </Stack.Item>
+                        )}
+                    </Tooltip>
+                    <Tooltip text="Analytics" position='end'>
+                        { (msg)=> (
+                        <Stack.Item attributes={msg}>
+                            <Link href="/v3/analytics">
+                                <Button color={path==="/v3/analytics" ? "primary" : "disabled"}  variant={path==="/v3/analytics" ?  "" :"outline" } fullWidth>
+                                    Analytics
+                                </Button>
+                            </Link>
+                        </Stack.Item>
+                        )}
+                    </Tooltip>
+                    <Tooltip text="Notebooks" position='end'>
+                        { (msg)=> (
+                        <Stack.Item attributes={msg}>
+                            <Link href="/v3/notebooks">
+                                <Button color={path=== "/v3/notebooks" ? "primary" : "neutral"}  variant={path=== "/v3/notebooks" ?  "" :"outline" } fullWidth>
+                                    Notebooks
+                                </Button>
+                            </Link>
+                        </Stack.Item>
+                        )}
+                    </Tooltip>
+                    <Tooltip text="Profile" position='end'>
+                        { (msg)=> (
+                        <Stack.Item attributes={msg}>
+                            <Link href="/v3/profile">
+                                <Button color={path=== "/v3/profile" ? "primary" : "neutral"}  variant={path=== "/v3/profile" ?  "" :"outline" } fullWidth>
+                                    Profile
+                                </Button>
+                            </Link>
+                        </Stack.Item>
+                        )}
+                    </Tooltip>
                 </Stack>
             </ActionBar>
         </Frame>
