05ce572d2682b30ed9bc14d12fc4e1d229dce324
gyanAsh <gyanash36@protonmail.com>
2022-09-18 16:05:42 +0530

    Replaced const value to a state :Bug

--- components/Wrapper/Navigation.js
+++ components/Wrapper/Navigation.js
@@ -1,13 +1,21 @@
+import { useState,useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link'
-// import Image from 'next/image'
 import { ActionBar, Button, Image, Frame, Stack } from "reshaped"
 import style from '../../styles/Navigation.module.css'
+
 const Navigation = () => {
+    const [path, setPath] = useState([]);
     const router = useRouter();
     const regex = /^(\/v3\/\b(home|packs|tests|analytics|notebooks|profile)\b)/;
-    const pathInfo = router.asPath.match(regex);
-    const path = pathInfo ? pathInfo[0] : null;
+    let pathInfo = router.asPath.match(regex);
+    useEffect(() => {
+      if (pathInfo)
+        setPath(pathInfo[0]);
+    else
+        setPath(null);
+    }, [pathInfo])
+    
     return (
         <Frame
             className={style.navbar}
