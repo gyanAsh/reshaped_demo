749024c12bd6cb31a5639e74f93c7a9de1cb368f
gyanAsh <gyanash36@protonmail.com>
2022-09-24 16:08:54 +0530

    Minor css/style changes mand to span tag

--- components/Home/GreetingTitle.js
+++ components/Home/GreetingTitle.js
@@ -5,7 +5,7 @@ import { Text } from "reshaped";
 const GreetingTitle = () => {
   return (
       <Fragment>
-          <Text className={style.mainTitle} variant="display-3">Good Evening&#44; <span>Gyan Ashish Ekka&#33;</span></Text>
+          <Text className={style.mainTitle} variant="display-3">Good Evening&#44; <Text color="primary" as="span">Gyan Ashish Ekka&#33;</Text></Text>
           <Text variant="featured-2">Welcome back&#33; All the best because &#35;PaperPhodnaHai</Text>
       </Fragment>
   )
